4b01440aaab343c15ef733e77d11948b8ec8a5df
xaxxon <colin@xaxxon.com>
2015-11-19 13:43:19 -0800

    arcmove tweaks

--- src/arcmove_globalpath_follower.py
+++ src/arcmove_globalpath_follower.py
@@ -24,7 +24,7 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from actionlib_msgs.msg import GoalStatusArray
 from move_base_msgs.msg import MoveBaseActionGoal
 
-listentime = 0.6 # 0.6 # allows odom + amcl to catch up
+listentime = 0.1 # 0.6 # allows odom + amcl to catch up
 nextmove = 0
 odomx = 0
 odomy = 0
@@ -45,16 +45,28 @@ lastpath = 0  # refers to localpath
 goalpose = False
 goalseek = False
 meterspersec = 0.33 # linear speed  TODO: get from java
+radianspersec = 1.496
 tfth = 0
-globalpathposenum = 20 # just right
+globalpathposenum = 20  
 listener = None
 
 
 def pathCallback(data): # local path
 	global goalpose, lastpath
+	global targetx, targety, targetth 
 	
 	lastpath = rospy.get_time()
 	goalpose = False
+	
+	# if initialturn:
+		# return	
+		
+	# p = data.poses[len(data.poses)-1] # get latest pose
+	# targetx = p.pose.position.x
+	# targety = p.pose.position.y
+	# quaternion = ( p.pose.orientation.x, p.pose.orientation.y,
+	# p.pose.orientation.z, p.pose.orientation.w )
+	# targetth = tf.transformations.euler_from_quaternion(quaternion)[2]
 	
 def globalPathCallback(data):
 	global targetx, targety, targetth , followpath, pathid
@@ -62,6 +74,11 @@ def globalPathCallback(data):
 	n = len(data.poses)
 	if n < 5:
 		return
+
+	followpath = True
+
+	# if not initialturn:
+		# return
 		
 	if n-1 < globalpathposenum:
 		p = data.poses[n-1] 
@@ -74,7 +91,6 @@ def globalPathCallback(data):
 	p.pose.orientation.z, p.pose.orientation.w )
 	targetth = tf.transformations.euler_from_quaternion(quaternion)[2]
 	
-	followpath = True
 	pathid = data.header.seq
 
 def odomCallback(data):
@@ -144,25 +160,54 @@ def arcmove(ox, oy, oth, tx, ty, tth, gth):
 		-if angle > 90? do linear move instead then wait (stopbetweenmoves true first?)
 	"""
 	
-	global initialturn, waitonaboutface
-	global followpath, goalpose, tfth, pathid
-	global odomx, odomy, odomth
+	global initialturn, waitonaboutface, nextmove
+	# global followpath, goalpose, tfth, pathid
+	# global odomx, odomy, odomth
 	
 	# find arclength if any, and turn radians, depending on scenario
 	arclength = 0
 	goalrotate = False	
-	if followpath and not ox==tx and not oy==ty: # normal arc move
-		# distance > 0, determine arc length here
-		distance = math.sqrt( pow((tx - ox),2) + pow((ty - oy),2) )
-		dth = tth - oth
+	if followpath and not ox==tx and not oy==ty and not initialturn: # normal arc move
+		dx = tx - ox
+		dy = ty - oy	
+		distance = math.sqrt( pow(dx,2) + pow(dy,2) )
+		th = math.acos(dx/distance)
+		if dy <0:
+			th = -th
+
+		dth = th - oth
+		if dth > math.pi:
+			dth = -math.pi*2 + dth
+		elif dth < -math.pi:
+			dth = math.pi*2 + dth
+
 		radius = (distance/2)/math.sin(dth/2)
 		arclength = radius * dth # *should* work out to always be > 0
+		if abs(dth/arclength) > 2.5: #  1.57:
+			arclength = 0
+			print ("dpm > 70")
+		else: 
+			print("arclength>0")
 	elif goalpose:  # final goal rotate move
-		dth = (gth - tfth) - oth # goal pose is in tf coordinate space!
+		dth = (gth - tfth)-oth
 		goalrotate = True
-	else:  # initial turn move (always?)
-		dth = tth - oth
+		print("goalrotate")
+	else: # initial turn move (always?)  
+		dx = tx - ox
+		dy = ty - oy	
+		distance = math.sqrt( pow(dx,2) + pow(dy,2) )
+		th = math.acos(dx/distance)
+		if dy <0:
+			th = -th
 
+		dth = th - oth
+		if initialturn:
+			initialturn = False
+			print("initialturn")
+		else:
+			print("not initialturn")
+
+	# determine angle delta for move
 	if dth > math.pi:
 		dth = -math.pi*2 + dth
 	elif dth < -math.pi:
@@ -170,18 +215,13 @@ def arcmove(ox, oy, oth, tx, ty, tth, gth):
 		
 	# if turning more than 120 deg, inch forward, make sure not transient obstacle (like door transfer)
 	# TODO: skip if within 3 feet of goal?!
-	if abs(dth) > 2.0944 and not goalrotate and not initialturn and waitonaboutface < 1: 
-		oculusprimesocket.sendString("forward 0.25")
-		oculusprimesocket.waitForReplySearch("<state> direction stop")
-		waitonaboutface += 1 # only do this once
-		rospy.sleep(1)
-		return
-	waitonaboutface = 0
-	
-	# turn in place if > 70 deg
-	if arclength > 0 and abs(dth) > 1.22:   
-		initialturn = False
-		arclength = 0
+	# if abs(dth) > 2.0944 and not goalrotate and not initialturn and waitonaboutface < 1: 
+		# oculusprimesocket.sendString("forward 0.25")
+		# oculusprimesocket.waitForReplySearch("<state> direction stop")
+		# waitonaboutface += 1 # only do this once
+		# rospy.sleep(1)
+		# return
+	# waitonaboutface = 0
 
 	if arclength > 0: # arcmove
 		# force arclength limits	
@@ -191,7 +231,9 @@ def arcmove(ox, oy, oth, tx, ty, tth, gth):
 			arclength = maxarclinear
 
 		oculusprimesocket.sendString("arcmove " + str(distance) + " " + str(int(math.degrees(dth))) ) 
-		initialturn = False
+		rospy.sleep(distance/meterspersec)
+		print("arcmove " + str(distance) + " " + str(int(math.degrees(dth))) ) 
+		nextmove = rospy.get_time()
 		return
 	
 	# rotate only
@@ -206,9 +248,16 @@ def arcmove(ox, oy, oth, tx, ty, tth, gth):
 	if dth > 0:
 		oculusprimesocket.sendString("left " + str(int(math.degrees(dth))) ) 
 		oculusprimesocket.waitForReplySearch("<state> direction stop")
+		rospy.sleep(dth/radianspersec)
+		print ("left " + str(int(math.degrees(dth))) )
 	elif dth < 0:
 		oculusprimesocket.sendString("right " +str(int(math.degrees(-dth))) )
 		oculusprimesocket.waitForReplySearch("<state> direction stop")
+		rospy.sleep(dth/radianspersec)
+		print ("right " +str(int(math.degrees(-dth))) )
+		
+	nextmove = rospy.get_time() + 0.4
+	
 	
 		
 
@@ -318,10 +367,9 @@ while not rospy.is_shutdown():
 	t = rospy.get_time()
 	
 	if t >= nextmove:
-		# nextmove = t + listentime
 		if goalseek and (followpath or goalpose): 
 			arcmove(odomx, odomy, odomth, targetx, targety, targetth, goalth) # blocking
-			nextmove = rospy.get_time() + listentime
+			# nextmove = rospy.get_time() + listentime
 			followpath = False
 	
 	if t - lastpath > 3:
