845537ab09ea9aa88e3747f4e6116dcd9c0e213d
xaxxon <colin@xaxxon.com>
2017-10-03 14:18:08 -0700

    lidar testing

--- src/xaxxon_lidar.py
+++ src/xaxxon_lidar.py
@@ -15,7 +15,7 @@ def cleanup():
 
 rospy.init_node('xaxxon_lidar', anonymous=False)
 rospy.on_shutdown(cleanup)
-scan_pub = rospy.Publisher('scan', LaserScan, queue_size=10)
+scan_pub = rospy.Publisher('scan', LaserScan, queue_size=2)
 
 
 # connect
@@ -31,12 +31,13 @@ while ser.inWaiting() > 0:
 
 if not line == "<id::xaxxonlidar>":
 	rospy.signal_shutdown("incorrect board id")
+	print("incorrect board id")
 	ser.close()
 else:	
 	# start lidar	
 	ser.write("1\n") # enable lidar
 	ser.write("b\n") # enable broadcast
-	ser.write("g\n"); # start rotation
+	ser.write("g\n") # start rotation
 	# ser.write("m")
 	# ser.write(chr(99))
 	# ser.write("\n")
@@ -67,27 +68,28 @@ while not rospy.is_shutdown() and ser.is_open:
 	#read header cycle
 	low = ord(ser.read(1))
 	high = ord(ser.read(1))
-	cycle = (high<<8)|low
+	cycle = ((high<<8)|low)/1000.0
 
 	print "cycle: "+str(cycle)
 	print "count: "+str(count)
-	print "raw_data length: "+str(len(raw_data))
+	# print "raw_data length: "+str(len(raw_data))
+	print " "
 	
 	scannum += 1	
-	if scannum < 8: # drop 1st few scans while lidar spins up
+	if scannum < 11: # drop 1st few scans while lidar spins up
 		del raw_data[:]
 		continue
 	
 	current_time = rospy.Time.now()
 	scan = LaserScan()
 
-	scan.header.stamp = current_time
+	scan.header.stamp = current_time - rospy.Duration(cycle+.015)
 	scan.header.frame_id = 'laser_frame'
 	scan.angle_min = 0.0
 	scan.angle_max = 2 * math.pi
-	scan.angle_increment = 2 * math.pi / count
-	scan.time_increment = cycle/count/1000.0
-	scan.range_min = 0.1
+	scan.angle_increment = 2 * math.pi / (count+1)
+	scan.time_increment =  cycle/count
+	scan.range_min = 0.08
 	scan.range_max = 20.0
 
 	# scan.ranges[0:count-1] = raw_data[len(raw_data)-count-3:len(raw_data)-3]
